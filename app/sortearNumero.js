d90dd4102e26fdf2a9674e2eaa257173c76fa3d0
Daniel <danielgd.bh@gmail.com>
2023-03-02 23:02:37 -0300

    criando arquivo js para gerar numero aleatorio e alterar valores de maior e menor no html

---
+++ app/sortearNumero.js
@@ -0,0 +1,13 @@
+const maiorValor = 1000
+const menorValor = 1
+const numeroAleatorio = gerarNumeroAleatorio()
+
+function gerarNumeroAleatorio (){
+    return parseInt(Math.random()*maiorValor +1)
+}
+console.log(numeroAleatorio)
+
+const maior = document.querySelector('#maior-valor')
+maior.innerHTML = maiorValor
+const menor = document.querySelector('#menor-valor')
+menor.innerHTML = menorValor
